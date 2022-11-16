46e7c0c95524437697faa1929f65c9fecd44ffb6
Hilal Türker <hilal.turker@rasyotek.com.tr>
2022-11-16 11:23:55 +0300

    Add project files.

---
+++ AgoraDeneme/Pages/lobby.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgoraDeneme.Pages
+{
+    public class lobbyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
